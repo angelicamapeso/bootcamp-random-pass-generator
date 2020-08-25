389d01ff1d1898a260f5cbd5d8d364ae2f9e4dbf
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-08-24 22:40:51 -0400

    Create isPasswordLengthValid function

--- script.js
+++ script.js
@@ -17,13 +17,6 @@ function displayNewPassword () {
 
 /* Your solution code goes here ... */
 
-//object criteria needs to have:
-// min-length, max-length
-// lowercase
-// uppercase
-// numeric
-// special characters
-
 function getCriteria() {
   
   const userCriteria = {
@@ -69,15 +62,20 @@ function areCharactersValid(characterTypes) {
 
 function askPasswordLength() {
   let length = Number(prompt("How long would you like the password to be? (Must be a number from 8 to 128)"));
-  while (Number.isNaN(length)
-        || length % 1 !== 0
-        || length < 8 
-        || length > 128 ) {
+  while (!isPasswordLengthValid(length)) {
     //console.log("Invalid length: " + length);
     length = Number(prompt("Sorry, that is an invalid length. The length must be an integer from 8 to 128. How long would you like the password to be?"));
   }
   //console.log("Valid length: " + length);
   return length;
+}
+
+function isPasswordLengthValid(length) {
+  const validCondition = (!Number.isNaN(length)
+    && length % 1 === 0
+    && length >= 8 
+    && length <= 128); 
+  return validCondition;
 }
 
 function generatePassword(criteriaObject) {
